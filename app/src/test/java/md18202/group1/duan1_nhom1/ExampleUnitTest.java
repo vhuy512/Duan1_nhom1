effe3f365d9b42f63e6fad6ca1dc856ef7580789
huylvph30524 <vhuy887@gmail.com>
2023-08-05 21:11:12 +0700

    duan1

---
+++ app/src/test/java/md18202/group1/duan1_nhom1/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package md18202.group1.duan1_nhom1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
